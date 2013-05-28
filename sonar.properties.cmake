62aecac19ef423ebe8fcfab20fd572dfe194a68a
Paul-Christian Volkmer <pcvolkmer@linux-xl5s.site>
2013-05-28 15:31:38 +0200

    Add cmake template for sonar.properties

---
+++ sonar.properties.cmake
@@ -0,0 +1,20 @@
+# required metadata
+sonar.projectKey=de.thm.arsnova:ARSnovaDesktop
+sonar.projectName=ARSnovaDesktop
+sonar.projectVersion=@VERSION_MAJOR@.@VERSION_MINOR@.@VERSION_PATCH@
+  
+# path to source directories (required)
+sonar.sources=src
+
+# The value of the property must be the key of the language.
+sonar.language=c++
+
+sonar.cxx.suffixes.sources=cpp
+sonar.cxx.xunit.reportPath=*Test.xml
+sonar.cxx.coverage.reportPath=coverage.xml
+sonar.cxx.valgrind.reportPath=*Test.memcheck
+  
+sonar.scm.url=scm:git:git://scm.thm.de/arsnova/arsnovadesktop.git
+
+# exclude qt created files
+sonar.exclusions=**/ui_*.h
