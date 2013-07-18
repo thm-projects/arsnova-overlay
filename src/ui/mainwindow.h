3b1792b2c05247d83469652443ef8074695605e0
Paul-Christian Volkmer <paul-christian.volkmer@mni.thm.de>
2013-07-18 10:44:39 +0200

    Changed CMake config and code to be compatible with Qt5

--- src/ui/mainwindow.h
+++ src/ui/mainwindow.h
@@ -1,7 +1,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QtGui>
+#include <QtWidgets>
 
 #include "version.h"
 #include "ui_mainwindow.h"
--- src/ui/systemtrayicon.h
+++ src/ui/systemtrayicon.h
@@ -1,7 +1,7 @@
 #ifndef SYSTEMTRAYICON_H
 #define SYSTEMTRAYICON_H
 
-#include <QtGui>
+#include <QtWidgets>
 
 class SystemTrayIcon : public QSystemTrayIcon {
 
