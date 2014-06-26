cca98bcfd60e752c9d63659b45b13e08486ac968
Paul-Christian Volkmer <paul-christian.volkmer@mni.thm.de>
2014-06-26 17:15:57 +0200

    Replace project name in license header

--- src/settings.cpp
+++ src/settings.cpp
@@ -1,21 +1,21 @@
 /***************************************************************************
- *   Copyright (C) 2013 Paul-Christian Volkmer
+ *   Copyright (C) 2014  Paul-Christian Volkmer
  *   <paul-christian.volkmer@mni.thm.de>
  *
- *   This file is part of ARSnovaDesktop.
+ *   This file is part of ARSnova Overlay.
  *
- *   ARSnovaDesktop is free software: you can redistribute it and/or modify
+ *   ARSnova Overlay is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
  *   (at your option) any later version.
  *
- *   ARSnovaDesktop is distributed in the hope that it will be useful,
+ *   ARSnova Overlay is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU General Public License for more details.
  *
  *   You should have received a copy of the GNU General Public License
- *   along with ARSnovaDesktop.  If not, see <http://www.gnu.org/licenses/>.
+ *   along with ARSnova Overlay.  If not, see <http://www.gnu.org/licenses/>.
  ***************************************************************************/
 
 #include "settings.h"
@@ -23,7 +23,7 @@
 std::shared_ptr<Settings> Settings::_instance = std::shared_ptr<Settings> ();
 
 Settings::Settings() {
-    qsettings = new QSettings ( "THM", "ARSnovaDesktop" );
+    qsettings = new QSettings ( "THM", "ARSnova Overlay" );
     qsettings->setDefaultFormat ( QSettings::NativeFormat );
 }
 
--- src/ui/systemtrayicon.cpp
+++ src/ui/systemtrayicon.cpp
@@ -1,10 +1,30 @@
+/***************************************************************************
+ *   Copyright (C) 2014  Paul-Christian Volkmer
+ *   <paul-christian.volkmer@mni.thm.de>
+ *
+ *   This file is part of ARSnova Overlay.
+ *
+ *   ARSnova Overlay is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   ARSnova Overlay is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with ARSnova Overlay.  If not, see <http://www.gnu.org/licenses/>.
+ ***************************************************************************/
+
 #include "systemtrayicon.h"
 #include "mainwindow.h"
 
 SystemTrayIcon * SystemTrayIcon::_instance = nullptr;
 
 SystemTrayIcon::SystemTrayIcon ( QIcon icon ) : QSystemTrayIcon ( icon, QApplication::desktop() ) {
-    menu = new QMenu ( "ARSnovaDesktop" );
+    menu = new QMenu ( "ARSnova Overlay" );
     this->addExitAction();
     this->setContextMenu ( menu );
 }
